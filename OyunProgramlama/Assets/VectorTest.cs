d59a7f2b8da7f4681c915f9dd5ce171fb79b48af
firatatalay34 <firatatalay34@hotmail.com>
2020-04-25 03:39:50 +0300

    Cross Product
    Vektör çarpımı, çapraz çarpım

--- OyunProgramlama/Assets/VectorTest.cs
+++ OyunProgramlama/Assets/VectorTest.cs
@@ -7,6 +7,7 @@ public class VectorTest : MonoBehaviour
     public Transform p1Transform;
     public Transform p2Transform;
 
+    public Transform cube;
     Vector3 point1;
     Vector3 point2;
 
@@ -23,6 +24,28 @@ public class VectorTest : MonoBehaviour
         point1 = p1Transform.position;
         point2 = p2Transform.position;
 
+        Debug.DrawLine(Vector3.zero, point1, Color.red);
+        Debug.DrawLine(Vector3.zero, point2, Color.green);
+
+        float x = point1.y * point2.z - point1.z * point2.y;
+        float y = point1.z * point2.x - point1.x * point2.z;
+        float z = point1.x * point2.y - point1.y * point2.x;
+        Vector3 cross = new Vector3(x, y, z);
+
+        cube.Rotate(cross, 10);
+
+        //Vector3 cross = Vector3.Cross(point1,point2); Bu işlem ile direkt hazır bir metod ile cross işlemi yapılabilir.
+        Debug.DrawLine(Vector3.zero, cross, Color.yellow);
+
+        //cx = aybz − azby
+        //cy = azbx − axbz
+        //cz = axby − aybx
+
+        //NewMethod();
+    }
+
+    private void NewMethod()
+    {
         //float magnitute = Mathf.Sqrt(Mathf.Pow(point1.x, 2) + Mathf.Pow(point1.y, 2));
 
         //Vector3 unitPoint1 = point1 / point1.magnitude;
@@ -50,6 +73,6 @@ public class VectorTest : MonoBehaviour
         //Debug.DrawLine(Vector3.zero, point1-point2, Color.black);
 
 
-       // Debug.DrawLine(Vector3.zero, unitPoint1, Color.green); //Debug.Log(point1.normalized);
+        // Debug.DrawLine(Vector3.zero, unitPoint1, Color.green); //Debug.Log(point1.normalized);
     }
 }
