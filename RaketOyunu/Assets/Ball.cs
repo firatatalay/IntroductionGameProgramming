c64f368884204896e8cd03d4b7e0da170014b563
firatatalay34 <firatatalay34@hotmail.com>
2020-04-25 12:57:41 +0300

    RaketOyunu
    Duvarlar, raketler, toplar oluşturuldu. Top çarpma sesi eklendi. Score board aktifleştirildi.

---
+++ RaketOyunu/Assets/Ball.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Racket solRaket, sagRaket;
+
+    Rigidbody2D rb2;
+    public float moveSpeed=10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+        rb2.velocity = new Vector2(1, 0) * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TagManager tagManager = collision.gameObject.GetComponent<TagManager>();
+
+        GetComponent<AudioSource>().Play();
+
+        if (tagManager == null) return;
+        Tag tag = tagManager.myTag;
+
+        if(tag.Equals(Tag.SAG_DUVAR))
+        {
+            solRaket.SkorYap();
+        }
+
+        else if (tag.Equals(Tag.SOL_DUVAR))
+        {
+            sagRaket.SkorYap();
+        }
+
+
+        if (tag.Equals(Tag.SAG_RAKET))
+        {
+            DonusYonuHesaplama(collision, -1);
+        }
+        else if (tag.Equals(Tag.SOL_RAKET))
+        {
+            DonusYonuHesaplama(collision, 1);
+        }
+
+
+    }
+
+    private void DonusYonuHesaplama(Collision2D collision, int x)
+    {
+        float a = transform.position.y - collision.gameObject.transform.position.y;
+        float b = collision.collider.bounds.size.y;
+        float y = a / b;
+        rb2.velocity = new Vector2(x, y) * moveSpeed;
+    }
+}
---
+++ RaketOyunu/Assets/Racket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Racket : MonoBehaviour
+{
+    public string axisName = "Vertical1";
+    public float moveSpeed = 10;
+    public Text scoreText;
+
+    public int Score { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveAxis = Input.GetAxis(axisName) * moveSpeed;
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, moveAxis);
+    }
+
+    public void SkorYap()
+    {
+        Score++;
+        scoreText.text = Score.ToString();
+    }
+}
